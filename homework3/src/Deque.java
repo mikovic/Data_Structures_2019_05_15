9345beca34a806b9d14230201d05648d26d078b9
Konstantin Minakov <mikovic@yandex.ru>
2019-05-27 12:11:04 +0400

    lesson4

---
+++ homework3/src/Deque.java
@@ -0,0 +1,13 @@
+import queue.Queue;
+
+
+
+    public interface Deque<E> extends Queue<E> {
+
+        void insertLeft(E value);
+        void insertRight(E value);
+
+        E removeLeft();
+        E removeRight();
+    }
+
