ae550ab06e59852b18c7a03719f72cc5540c1f66
Konstantin Minakov <mikovic@yandex.ru>
2019-06-09 17:45:15 +0400

    ДЗ №8

--- Lesson_8/src/Main8.java
+++ Lesson_8/src/Main8.java
@@ -7,23 +7,28 @@ public class Main8 {
 //        HashTable hashTable = new HashTableImpl(5);//5 * 2 = 10
         HashTable hashTable = new DoubleHashTableImpl(5);//5 * 2 = 10
 
-        hashTable.put(new Item(1, "Orange"), 150);
-        hashTable.put(new Item(77, "Banana"), 100);
-        hashTable.put(new Item(60, "Lemon"), 250);
-        hashTable.put(new Item(21, "Potato"), 67);
-        hashTable.put(new Item(55, "Milk"), 120);
+        hashTable.put2(new Item(1, "Orange"), 150);
+        hashTable.put2(new Item(77, "Banana"), 100);
+        hashTable.put2(new Item(60, "Lemon"), 250);
+        hashTable.put2(new Item(21, "Potato"), 67);
+        hashTable.put2(new Item(55, "Milk"), 120);
 
         System.out.println("Size is " + hashTable.getSize());
         hashTable.display();
 
-        System.out.println("Cost potato is " + hashTable.get(new Item(21, "Potato")));
-        System.out.println("Cost banana is " + hashTable.get(new Item(77, "Banana")));
+        System.out.println("Cost potato is " + hashTable.get2(new Item(21, "Potato")));
+        System.out.println("Cost banana is " + hashTable.get2(new Item(77, "Banana")));
 
-        hashTable.remove(new Item(21, "Potato"));
-        hashTable.remove(new Item(77, "Banana"));
-        System.out.println("Cost banana is " + hashTable.get(new Item(77, "Banana")));
+
+        hashTable.remove2(new Item(77, "Banana"));
+        hashTable.remove2(new Item(21, "Orange"));
+
+        System.out.println("Cost banana is after removing " + hashTable.get2(new Item(77, "Banana")));
+
         hashTable.display();
-
+        System.out.println("Вставили апельсин опять. Поменялся местами с potato: ");
+        hashTable.put2(new Item(1, "Orange"), 150);
+        hashTable.display();
 
         Map<String, Integer> map = new HashMap<>();
         for (String s : map.keySet()) {
